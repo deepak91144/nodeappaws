5aff29acfce10147c400c5bd7d4a35fa3b8de532
deepak91144 <53552887+deepak91144@users.noreply.github.com>
2024-07-28 04:37:49 +0530

    fix: added morgan to app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const authRoutes = require("./route/auth.route");
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+app.use(authRoutes);
 app.get("/", (req, res) => {
 	console.log("working");
 	res.status(200).json({
